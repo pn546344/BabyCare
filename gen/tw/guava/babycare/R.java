3ebe5333ff230bf0876f2b4cb6bc8b1e5ee9f9ff
finch <pn546344@gmail.com>
2014-08-31 15:52:27 +0800

    CreateBaby畫面排版更新(增加難度選擇)

--- gen/tw/guava/babycare/R.java
+++ gen/tw/guava/babycare/R.java
@@ -30,8 +30,11 @@ public final class R {
     public static final class id {
         public static final int FrameLayout1=0x7f080000;
         public static final int LinearLayout1=0x7f08000a;
-        public static final int action_settings=0x7f08000e;
-        public static final int button1=0x7f08000d;
+        public static final int action_settings=0x7f080012;
+        public static final int button1=0x7f080011;
+        public static final int button2=0x7f08000e;
+        public static final int button3=0x7f08000f;
+        public static final int button4=0x7f080010;
         public static final int editText1=0x7f08000c;
         public static final int imageView1=0x7f080003;
         public static final int imageView2=0x7f080004;
@@ -43,6 +46,7 @@ public final class R {
         public static final int linearLayout2=0x7f080007;
         public static final int myView1=0x7f080001;
         public static final int textView1=0x7f08000b;
+        public static final int textView2=0x7f08000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
