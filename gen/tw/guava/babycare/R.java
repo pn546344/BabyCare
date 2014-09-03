8d59ce27389dea009c061f3fc8d952ac27d29c74
finch <pn546344@gmail.com>
2014-09-03 14:05:23 +0800

    1.新增食物介面的排版 2.主化面底部的功能列位置調整 3.新增互動功能的畫面

--- gen/tw/guava/babycare/R.java
+++ gen/tw/guava/babycare/R.java
@@ -30,7 +30,8 @@ public final class R {
     public static final class id {
         public static final int FrameLayout1=0x7f080000;
         public static final int LinearLayout1=0x7f080008;
-        public static final int action_settings=0x7f080013;
+        public static final int TableLayout1=0x7f080010;
+        public static final int action_settings=0x7f08001b;
         public static final int button1=0x7f08000f;
         public static final int button2=0x7f08000c;
         public static final int button3=0x7f08000d;
@@ -39,31 +40,40 @@ public final class R {
         public static final int imageView1=0x7f080003;
         public static final int imageView2=0x7f080004;
         public static final int imageView3=0x7f080005;
+        public static final int imageView4=0x7f080013;
+        public static final int imageView5=0x7f080014;
+        public static final int imageView6=0x7f080015;
         public static final int imageView7=0x7f080006;
         public static final int imageView8=0x7f080007;
+        public static final int imageView9=0x7f080017;
         public static final int linearLayout2=0x7f080002;
         public static final int myView1=0x7f080001;
-        public static final int numberPicker1=0x7f080010;
-        public static final int numberPicker2=0x7f080012;
+        public static final int numberPicker1=0x7f080018;
+        public static final int numberPicker2=0x7f08001a;
+        public static final int tableRow2=0x7f080011;
+        public static final int tableRow3=0x7f080012;
+        public static final int tableRow4=0x7f080016;
         public static final int textView1=0x7f080009;
         public static final int textView2=0x7f08000b;
-        public static final int textView3=0x7f080011;
+        public static final int textView3=0x7f080019;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int create_baby=0x7f030001;
         public static final int food=0x7f030002;
-        public static final int nursery=0x7f030003;
-        public static final int nursery_center=0x7f030004;
-        public static final int nursery_people=0x7f030005;
+        public static final int interactive=0x7f030003;
+        public static final int nursery=0x7f030004;
+        public static final int nursery_center=0x7f030005;
+        public static final int nursery_people=0x7f030006;
     }
     public static final class menu {
         public static final int create_baby=0x7f070000;
         public static final int food=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int nursery=0x7f070003;
-        public static final int nursery_center=0x7f070004;
-        public static final int nursery_people=0x7f070005;
+        public static final int interactive=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int nursery=0x7f070004;
+        public static final int nursery_center=0x7f070005;
+        public static final int nursery_people=0x7f070006;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -71,6 +81,7 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int title_activity_create_baby=0x7f050003;
         public static final int title_activity_food=0x7f050004;
+        public static final int title_activity_interactive=0x7f050008;
         public static final int title_activity_nursery=0x7f050005;
         public static final int title_activity_nursery_center=0x7f050006;
         public static final int title_activity_nursery_people=0x7f050007;
--- src/tw/guava/babycare/MainActivity.java
+++ src/tw/guava/babycare/MainActivity.java
@@ -95,8 +95,8 @@ public class MainActivity extends Activity implements OnClickListener {
 			intent = new Intent(this,Food.class);
             startActivity(intent);
 			break;
-		case R.id.imageView2:	//imageView2被按下時跳轉畫面到Food頁面
-			intent = new Intent(this,NurseryCenter.class);
+		case R.id.imageView2:	//imageView2被按下時跳轉畫面到Interactive頁面
+			intent = new Intent(this,Interactive.class);
             startActivity(intent);
 			break;
 		case R.id.imageView3:	//imageView3被按下時跳轉畫面到Food頁面
