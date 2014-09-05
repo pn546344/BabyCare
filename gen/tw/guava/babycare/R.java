bc3098cdec8f6b7c76cbeea9ce2b6ed4f53ca79e
finch <pn546344@gmail.com>
2014-09-05 17:30:05 +0800

    新增三張功能按鈕的圖(奶瓶,奶嘴,床) 調整好圖片顯示的大小

--- gen/tw/guava/babycare/R.java
+++ gen/tw/guava/babycare/R.java
@@ -24,8 +24,11 @@ public final class R {
     public static final class drawable {
         public static final int baby1=0x7f020000;
         public static final int baby2=0x7f020001;
-        public static final int createbaby=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int bed=0x7f020002;
+        public static final int createbaby=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int milk=0x7f020005;
+        public static final int pacifier=0x7f020006;
     }
     public static final class id {
         public static final int FrameLayout1=0x7f080000;
