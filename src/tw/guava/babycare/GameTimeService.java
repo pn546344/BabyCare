e6f5a4c56181dc5db07539480f95dcc5c13714b4
finch <pn546344@gmail.com>
2014-09-11 15:43:52 +0800

    修正Service

--- src/tw/guava/babycare/GameTimeService.java
+++ src/tw/guava/babycare/GameTimeService.java
@@ -4,18 +4,22 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-public class Timer extends Service{
+public class GameTimeService extends Service{
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	@Override
-	public void onCreate() {
-		// TODO Auto-generated method stub
-		super.onCreate();
-	}
 
+	class Timer implements Runnable
+	{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
